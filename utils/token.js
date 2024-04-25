822351ed7dc0673f46cb573a0b29162a53ad1777
Rohith Raj Bingi <rohithrajb@gmail.com>
2024-04-25 23:47:36 +0530

    Added expiry to token

--- utils/token.js
+++ utils/token.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
 
-const accessToken = (userData) => jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET);
+const accessToken = (userData) => jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10d'});
 
 module.exports = accessToken;
